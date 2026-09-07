23bd0fb32461cefa2586b01db255f16bd9beaa54
Aindriu Lavelle <aindriu.lavelle@aiven.io>
2026-09-07 11:16:12 +0100

    Update Changelog and alter release script
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

--- scripts/release_detail.sh
+++ scripts/release_detail.sh
@@ -60,9 +60,3 @@ git checkout -b changelog-${endVersion}
 git add CHANGE_LOG.md
 git commit -m "Changelog for ${startTag} to v${endVersion}"
 git push --set-upstream origin changelog-${endVersion}
-
-mvn -P pre-release-check verify
-if [[ $? -eq 1 ]]
-then
-  echo "Fix issues with the build and rerun 'mvn -P pre-release-check verify'"
-fi
